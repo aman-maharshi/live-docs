3c3a4681f63179d96f2f719595f95cc8ab75398e
aman-maharshi <amanm4harshi@gmail.com>
2024-07-21 19:50:40 +0530

    editor init

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,12 @@
+import { Editor } from '@/components/editor/Editor'
 import React from 'react'
 
 const SingleDocument = () => {
   return (
-    <div className='p-4 md:px-14'>Single Document</div>
+    <div className='p-4 md:px-14'>
+      <div>Single Document</div>
+      <Editor />
+    </div>
   )
 }
 
