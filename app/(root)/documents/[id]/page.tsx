f3903189492258ef2ebe1198284a2c105bb541dc
aman-maharshi <amanm4harshi@gmail.com>
2024-07-23 01:13:39 +0530

    header and types init

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,13 @@
 import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
 import React from 'react'
 
 const SingleDocument = () => {
   return (
-    <div className='p-4 md:px-14'>
-      <div>Single Document</div>
+    <div>
+      <Header>
+        <p className='text-white'>Test</p>
+      </Header>
       <Editor />
     </div>
   )
