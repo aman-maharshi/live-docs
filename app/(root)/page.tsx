10a167668432ba79820440e9868835346c1af65a
aman-maharshi <amanm4harshi@gmail.com>
2024-07-26 01:59:56 +0530

    liveblocks init

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-4 md:px-14'>
-      <div className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ab!</div>
-      <Button>Click me</Button>
+    <div>
+      <Header>
+        Hi
+      </Header>
+      <div className='px-4 md:px-8'>
+        <div className='mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, ab!</div>
+        <Button>Click me</Button>
+      </div>
 
     </div>
   )
---
+++ app/Provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ReactNode } from "react"
+import { LiveblocksProvider, ClientSideSuspense } from "@liveblocks/react/suspense"
+
+const Provider = ({ children }: { children: ReactNode }) =>{
+  return (
+    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        {children}
+      </ClientSideSuspense>
+    </LiveblocksProvider>
+  )
+}
+
+export default Provider
