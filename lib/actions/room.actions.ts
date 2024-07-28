ed2ca26bfad563978c315d0831de76a3295dee36
aman-maharshi <amanm4harshi@gmail.com>
2024-07-29 02:21:36 +0530

    server action to create room | document

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import { revalidatePath } from "next/cache";
+import { liveblocks } from "../liveblocks";
+import { nanoid } from 'nanoid'
+import { parseStringify } from "../utils";
+
+export const createDocument = async ({userId, email}: CreateDocumentParams) => {
+  const roomId = nanoid()
+
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: "Untitled Document",
+    }
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"]
+    }
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: []
+    })
+
+    revalidatePath('/')
+
+    return parseStringify(room)
+
+
+  } catch (error) {
+    console.log("Error occurred while creating a room", error)
+  }
+}
